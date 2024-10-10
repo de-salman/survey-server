bb9a3fb2ea5626d49cdfb6a097794dedfc48eeb8
de-salman <salmanfarisi.bca@gmail.com>
2024-10-10 07:35:20 +0530

    new commit

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(
-    "mongodb+srv://hopsies:hopsies@ecomerce.ycr7xsn.mongodb.net/survey?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
